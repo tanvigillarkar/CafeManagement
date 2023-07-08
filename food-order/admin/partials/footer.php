c9786d84da29df062c2a40baaa10d486894ba5bf
tanvigillarkar <90440251+tanvigillarkar@users.noreply.github.com>
2023-07-08 19:06:44 +0530

    Add files via upload

---
+++ food-order/admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center"> All rights reserved, Coffee with Tanvi. Developed By - <a href="#"> Tanvi Gillarkar</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
---
+++ food-order/partials-front/footer.php
@@ -0,0 +1,28 @@
+<!-- social Section Starts Here -->
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="https://www.facebook.com/tanvi.gillarkar?mibextid=ZbWKwL"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="https://instagram.com/tanvigillarkar?igshid=ZDdkNTZiNTM="><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/tanvi-gillarkar-2a7788215"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="#">Tanvi Gillarkar</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
